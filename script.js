f5c992cfcd0e0e9efd4bccaa652cf7e3f6e75e2d
SujayBWJ <sujaybharadwaj.dev@gmail.com>
2026-01-16 16:11:31 +0530

    fixed delete issue

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+let currentIndex = null;
 const del = document.getElementById("delBtn")
 const save = document.getElementById('saveBtn')
 const noter = document.getElementById('noter')
 const show = document.getElementById('show')
 const list = document.getElementById("list");
 const titleInput = document.getElementById("title");
+
 
 
 save.addEventListener('click', () =>{
@@ -16,12 +18,14 @@ const note = {
     notes.push(note) // now push the title into the notes array
     localStorage.setItem("notes", JSON.stringify(notes)) // now store the modified notes array inside localstorage by converting it back to string
 
+    if(save) alert("Your note has been saved!")
+
 });
 
 show.addEventListener("click", ()=>{
     const notes = JSON.parse(localStorage.getItem("notes")) || []
     list.innerHTML = "";
-    notes.forEach(n => {  // n stores text of the note the forEach is iterating over
+    notes.forEach((n,i) => {  // n stores text of the note the forEach is iterating over
 
         const item = document.createElement("div")
         item.textContent = n.title; // store the title of the note inside item
@@ -29,6 +33,16 @@ show.addEventListener("click", ()=>{
         item.addEventListener("click", () =>{
             titleInput.value = n.title;
             noter.value = n.body;
+            currentIndex = i;
+
+            // del.onclick = () => {
+            //     const updated = JSON.parse(localStorage.getItem("notes")) || [];
+            //     updated.splice(i, 1);
+            //     localStorage.setItem("notes", JSON.stringify(updated));
+            //     list.innerHTML = "";
+            //     titleInput.value = "";
+            //     noter.value = "";
+            // };
         })
         list.appendChild(item) // put the content of item into the list
 
@@ -38,7 +52,14 @@ show.addEventListener("click", ()=>{
 
 
 del.addEventListener("click", ()=>{
-    localStorage.removeItem("notes");
+    if(currentIndex === null) return;
+    const notes = JSON.parse(localStorage.getItem("notes")) || []
+    notes.splice(currentIndex,1);
+    localStorage.setItem("notes", JSON.stringify(notes))
+    // localStorage.removeItem("notes");
+
+    currentIndex = null
+    titleInput.value = " "
     list.innerHTML = ""
     noter.value = "";
 });
